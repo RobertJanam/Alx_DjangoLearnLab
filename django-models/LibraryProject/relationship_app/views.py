f09bf921f1c7f045a9f5f3901e422d192bddd0b9
RobertJanam <robertbjanam@gmail.com>
2026-01-17 22:21:44 +0300

    added template name for LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Library, Book
 from django.views.generic import DetailView
 
 # Create your views here.
 def display_all_books(request):
     all_books = Book.objects.all()
-    context = {"all books" : all_books}
+    context = {"all books": all_books}
 
     return render(request, "relationship_app/list_books.html", context)
 
 class Display_all_books_in_aLibrary(DetailView):
     model = Library
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
