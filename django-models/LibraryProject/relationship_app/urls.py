81b146d5f686e2c2a233152b3746f8ac456463cd
RobertJanam <robertbjanam@gmail.com>
2026-01-18 14:52:12 +0300

    oops forgot template_name=

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view('relationship_app/library_detail.html'), name='library_detail'),
-    path('login/', LoginView.as_view('relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view('relationship_app/logout.html'), name='logout'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(template_name='relationship_app/library_detail.html'), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name="register"),
 
 ]
