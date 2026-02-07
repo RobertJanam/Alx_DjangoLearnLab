ef63658c839ecb9be320facc6da3a573c1922bb8
RobertJanam <robertbjanam@gmail.com>
2026-02-07 11:37:29 +0300

    urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,11 +9,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Book endpoints
+    # Book endpoints - following the exact patterns required
     path('books/', views.BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/update/', views.CustomBookUpdateView.as_view(), name='book-update'),
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
     path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
 
     # Author endpoints
     path('authors/', views.AuthorListView.as_view(), name='author-list'),
