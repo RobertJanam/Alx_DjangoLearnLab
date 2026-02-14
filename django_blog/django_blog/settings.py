8931cd6624887fb0d5ac5c7a19d6ab45b503ff8f
RobertJanam <robertbjanam@gmail.com>
2026-02-14 21:41:56 +0300

    Checks for the implementation of the static files for login, and register.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
