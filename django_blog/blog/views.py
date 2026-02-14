4d460cad25b01a4fa91d571624e509ec82830073
RobertJanam <robertbjanam@gmail.com>
2026-02-14 23:00:11 +0300

    updated urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -233,13 +233,12 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
+        form.instance.post_id = self.kwargs['pk']  # Changed from 'post_id' to 'pk'
         messages.success(self.request, 'Your comment has been added!')
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'pk': self.kwargs['post_id']})
-
+        return reverse('post-detail', kwargs={'pk': self.kwargs['pk']})
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """View to update an existing comment."""
